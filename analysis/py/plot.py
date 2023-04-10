f7136e7af45831a526acf78e3fde342e09775b6a
J Kunegis <kunegis@gmail.com>
2023-04-10 19:44:16 +0200

    separate the three plot types

--- analysis/py/plot.py
+++ analysis/py/plot.py
@@ -40,15 +40,11 @@ def plot_event(event, values, date_first):
     # dv - All by day
     #
     x= np.array([date_last + datetime.timedelta(days=xx) for xx in range(-n+1,1)])
-#    x= np.array(range(n))
     fig, ax= plt.subplots()
     ax.bar(x, values)
-#    ax.bar(x-len(x)+1, values)
     plt.title(f'{label}, by day, full range')
     plt.xlim([x[0] - datetime.timedelta(days= 1), x[-1] + datetime.timedelta(days= 1)])
-#    plt.xlim([-len(x)+1/2, 1/2])
     set_ylim(plt, values)
-#    plt.xlabel('Day (0 = today)')
     plt.ylabel(events.get_ylabel(event, 'dv'))
     ax.xaxis.set_major_locator(matplotlib.dates.YearLocator(5))
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%Y'))
